48d6480e8e45f908937593d363ae059486bfabdf
Samuel Rostand <85579646+Rostand-samuel@users.noreply.github.com>
2021-12-05 14:00:30 +0100

    Add files via upload

---
+++ restaurant1/restaurant.js
@@ -0,0 +1,67 @@
+window.addEventListener("load", function(){
+	/*----Animation on scroll-----*/
+	AOS.init();
+});
+
+
+
+
+
+
+
+/*----------barre de navigation------*/
+
+
+const navToggler = document.querySelector(".nav-toggler"); 
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+}
+/*--------fermer la barre de navigation  quand on clique dessus-----*/
+ document.addEventListener("click", function (e){
+     if (e.target.closest(".nav-item")){
+        toggleNav(); 
+     }
+     
+ });
+
+ /*--------sticky header--------*/
+
+ window.addEventListener("scroll", function(){
+ 	if (this.pageYOffset > 60){
+ 		document.querySelector(".header").classList.add("sticky");
+ 		}
+ 		else{
+ 			document.querySelector(".header").classList.remove("sticky");
+ 		}
+ });
+
+
+/*---------------------table menu-------------------------*/
+
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function(e){
+	if (e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active"))
+	{
+		const target = e.target.getAttribute("data-target");
+		menuTabs.querySelector(".active").classList.remove("active");
+		e.target.classList.add("active");
+		const menuSection = document.querySelector(".menu-section");
+		menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+		menuSection.querySelector(target).classList.add("active"); 
+		
+	 }	
+});
+
+
+
+
+
+
+
+
+
+
+
